6c325529edfd141e104d0ab663e2ae396e9044cc
Atsuhiro <atcheri@gmail.com>
2024-11-11 23:30:44 +0100

    feat: add auth.js to project
    And configured it to use github

---
+++ src/auth.ts
@@ -0,0 +1,7 @@
+import { AUTH_GITHUB_ID, AUTH_GITHUB_SECRET } from '$env/static/private';
+import { SvelteKitAuth } from '@auth/sveltekit';
+import GitHub from '@auth/sveltekit/providers/github';
+
+export const { handle, signIn, signOut } = SvelteKitAuth({
+	providers: [GitHub({ clientId: AUTH_GITHUB_ID, clientSecret: AUTH_GITHUB_SECRET })]
+});
