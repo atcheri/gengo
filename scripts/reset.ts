e9e9b0de8921fd67c42314ebdd7a3505728eb7dc
Atsuhiro <atcheri@gmail.com>
2024-11-13 09:22:27 +0100

    feat: add seed and reset typescript scripts

---
+++ scripts/reset.ts
@@ -0,0 +1,24 @@
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/neon-http';
+import { neon } from '@neondatabase/serverless';
+
+import * as schema from '../src/lib/server/db/schema';
+
+const sql = neon(process.env.DATABASE_URL!);
+const db = drizzle(sql, { schema });
+
+const main = async () => {
+	try {
+		console.log('Resetting the database');
+
+		await db.delete(schema.courses);
+		await db.delete(schema.userProgress);
+
+		console.log('Resetting finished');
+	} catch (error) {
+		console.error(error);
+		throw new Error('Failed to reset the database');
+	}
+};
+
+main();
